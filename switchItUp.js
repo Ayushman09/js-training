6f983ed95f25d604b484188c2a90120f2d83c464
Ayushman09 <ayushmanchak@gmail.com>
2021-04-29 17:30:17 +0530

    Assignment 6

---
+++ switchItUp.js
@@ -0,0 +1,11 @@
+var colour=prompt("Enter any color: ")
+switch(colour)
+{
+    case "blue":
+        console.log("Voilets are blue!")
+        break
+    case "red":
+        console.log("Rosesare red!")
+    default:
+        console.log("Everything is nice!")
+}
