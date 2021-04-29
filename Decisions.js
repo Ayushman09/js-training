0ddc5794d609de8a15049afe219aa8b85bcdca47
Ayushman09 <ayushmanchak@gmail.com>
2021-04-29 11:21:29 +0530

    Assignment 3

---
+++ Decisions.js
@@ -0,0 +1,29 @@
+var gift = prompt("What anniversary gift do you want?")
+var ans = gift.toLowerCase()
+if( ans=="roses"){
+    console.log("Happy Anniversary, here's what you wanted:  "+gift)
+}
+else if (ans == "jewelry"){
+    console.log("Happy Anniversary, here's what you wanted:  "+gift)
+}
+else if (ans == "card"){
+    console.log("Happy Anniversary, here's what you wanted:  "+gift)
+}
+else if (ans == "money"){
+    console.log("Happy Anniversary, here's what you wanted:  "+gift)
+}
+else if (ans == "candy"){
+    console.log("Happy Anniversary, here's what you wanted:  "+gift)
+}
+else if (ans == "electronics"){
+    console.log("Happy Anniversary, here's what you wanted:  "+gift)
+}
+else if (ans == "books"){
+    console.log("Happy Anniversary, here's what you wanted:  "+gift)
+}
+else if (ans == "puppy"){
+    console.log("Happy Anniversary, here's what you wanted:  "+gift)
+}
+else{
+    console.log("Happy Anniversary, Sorry I couldn't find what you wanted.")
+}
