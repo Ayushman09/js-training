60ba739fc973e91e587e3a5034156c8f21a7256f
Ayushman09 <ayushmanchak@gmail.com>
2021-05-03 08:24:04 +0530

    Assignment 12

---
+++ The DOM/project.js
@@ -0,0 +1,5 @@
+document.getElementById("h1").innerHTML = "<b>Welcome to my site!</b>";
+document.getElementById("p1").innerText = "My name is Ayushman";
+document.getElementById("h2").innerHTML = "";
+document.getElementById("p2").style.color = "blue";
+document.getElementById("p2").innerText= "I love JavaScript";
