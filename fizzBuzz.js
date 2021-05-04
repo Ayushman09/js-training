9a480752cedb228f19cfcb14a03925272fc07c01
Ayushman09 <ayushmanchak@gmail.com>
2021-05-04 11:07:10 +0530

    Assignment 19

---
+++ fizzBuzz.js
@@ -0,0 +1,30 @@
+function checkFizzBuzz(lowerRange,upperRange)
+{
+    for(var iterator=lowerRange;iterator<upperRange;iterator++)
+    {
+        if(iterator%3==0)
+        {
+            if(iterator%5==0)
+            {
+                console.log("FizzBuzz")
+            }
+            else{
+                console.log("Fizz")
+            }
+        }
+        else if(iterator%5==0){
+            if(iterator%3==0)
+            {
+                console.log("FizzBuzz")
+            }
+            else{
+                console.log("Buzz")
+            }
+        }
+        else{
+            console.log("The number is "+iterator)
+        }
+    }
+}
+
+checkFizzBuzz(3,18)
